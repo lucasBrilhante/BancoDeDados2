29b8032c333532bb63168b68e91c728fef949c92
Lucas Brilhante <lbrilhante82@gmail.com>
2016-05-18 13:32:06 -0300

    first files

---
+++ query/dvdRental_2.sql
@@ -0,0 +1,8 @@
+﻿SELECT *
+FROM (SELECT a.first_name,a.last_name,count(*) AS count
+FROM film_actor fa,
+actor a
+WHERE fa.actor_id = a.actor_id
+GROUP BY a.first_name,a.last_name) a
+WHERE a.count > 4
+ORDER BY a.count desc
